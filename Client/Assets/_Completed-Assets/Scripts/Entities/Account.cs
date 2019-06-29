de364d577df89dfddb112d3ac994c9dfce83c31f
canaan <it_wjw@163.com>
2019-06-29 18:14:15 +0800

    task 01: 连接到服务器，创建账号登录

---
+++ Client/Assets/_Completed-Assets/Scripts/Entities/Account.cs
@@ -0,0 +1,19 @@
+﻿using KBEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Account : AccountBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Account::Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Client/Assets/_Completed-Assets/Scripts/UI/UIEventControl.cs
@@ -0,0 +1,48 @@
+﻿using KBEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEventControl : MonoBehaviour
+{
+    public GameObject loginAccount;
+    public GameObject loginPwd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 登录按钮触发事件
+    /// </summary>
+    public void OnLoginClick()
+    {
+        if (loginAccount == null || loginPwd == null)
+        {
+            Debug.LogError("loginAccount or loginPwd is null");
+            return;
+        }
+
+        string account = loginAccount.GetComponent<InputField>().text;
+        string pwd = loginPwd.GetComponent<InputField>().text;
+
+        if (account.Length <= 3 || pwd.Length <= 3)
+        {
+            Debug.LogError("account or pwd invalid");
+            return;
+        }
+
+        Debug.Log("login, account: " + account + "pwd: " + pwd);
+
+        KBEngineApp.app.login(account, pwd, System.Text.Encoding.UTF8.GetBytes("kbengine_unity3d_demo"));
+    }
+}
